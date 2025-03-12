93200eab105ed30fa793d883368fcddef7f276c8
rfm-369 <202481583+rfm369@users.noreply.github.com>
2025-03-11 21:43:09 -0300

    1.0

---
+++ script/calculadora.js
@@ -0,0 +1,75 @@
+let tela = 0
+let calculated = false
+document.getElementById('tela').innerHTML = tela
+
+
+
+function displaynum(n){
+    // Função para exibir os números digitados no elemento com id "tela"
+    tela = document.getElementById('tela').innerHTML
+    if(tela == 0){
+        document.getElementById('tela').innerHTML = String(n)       
+    }
+    else if (calculated == true){
+        document.getElementById('tela').innerHTML = String(n)
+        calculated = false
+    }
+    else {
+        document.getElementById('tela').innerHTML += String(n)
+    }
+}
+function clear_c(){
+    // Função para limpar os números do elemento com id "tela"
+    tela = 0
+    document.getElementById('tela').innerHTML = tela
+}
+
+function clear_ce(){
+    // Função para limpar o último números digitados no elemento com id "tela"
+    tela = document.getElementById('tela').innerHTML.trim()
+    if (tela != 0){
+        if (tela.length == 1){
+            document.getElementById('tela').innerHTML = 0
+        }
+        else {
+        document.getElementById('tela').innerHTML = tela.slice(0,-1)
+    }
+    
+    }
+}
+
+function displayop(operator){
+
+    // Função para exibir os operadores aritiméticos
+    // digitados no elemento com id "tela"
+
+    tela = document.getElementById('tela').innerHTML
+    
+    if ("+-*/".includes(operator.trim().slice(-1)) && !"+-*/".includes(tela.trim().slice(-1))){
+        if (tela == 0){
+            document.getElementById('tela').innerHTML = operator
+        }
+        else {
+            document.getElementById('tela').innerHTML += String(operator)
+        }
+        
+    }
+    else if ("()".includes(operator.trim().slice(-1)) && !"()".includes(tela.trim().slice(-1))){
+        if(tela == 0){
+            document.getElementById('tela').innerHTML = operator
+        }
+        else {
+            document.getElementById('tela').innerHTML += String(operator)
+        }
+    }
+    
+    calculated = false
+
+}
+function calc(){
+    // Função para calcular o resultado e exibir no elemento com id "tela"
+    tela = document.getElementById('tela').innerHTML
+    result = eval(tela)
+    document.getElementById('tela').innerHTML = result
+    calculated = true
+}
