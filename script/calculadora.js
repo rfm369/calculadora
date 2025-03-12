e878e75bf214a4838811468cd9b89801c7de433c
rfm369 <rfidelesm@gmail.com>
2025-03-12 01:03:56 -0300

    Update calculadora.js

--- script/calculadora.js
+++ script/calculadora.js
@@ -1,45 +1,45 @@
 let screen = 0
 let calculated = false
-document.getElementById('screen').innerHTML = screen
+document.getElementById('tela').innerHTML = screen
 
 function limiter(){
-    // Função para limitar o número de caracteres no elemento com id "screen"
-    screen = document.getElementById('screen').innerHTML
+    // Função para limitar o número de caracteres no elemento com id "tela"
+    screen = document.getElementById('tela').innerHTML
     if (screen.length > 9){
-    document.getElementById('screen').innerHTML = screen.slice(0,9) 
+    document.getElementById('tela').innerHTML = screen.slice(0,9) 
 }
 }
 
 function displaynum(n){
-    // Função para exibir os números digitados no elemento com id "screen"
-    screen = document.getElementById('screen').innerHTML
+    // Função para exibir os números digitados no elemento com id "tela"
+    screen = document.getElementById('tela').innerHTML
     if(screen == 0){
-        document.getElementById('screen').innerHTML = String(n)       
+        document.getElementById('tela').innerHTML = String(n)       
     }
     else if (calculated == true){
-        document.getElementById('screen').innerHTML = String(n)
+        document.getElementById('tela').innerHTML = String(n)
         calculated = false
     }
     else {
-        document.getElementById('screen').innerHTML += String(n)
+        document.getElementById('tela').innerHTML += String(n)
     }
     limiter()
 }
 function clear_c(){
-    // Função para limpar os números do elemento com id "screen"
+    // Função para limpar os números do elemento com id "tela"
     screen = 0
-    document.getElementById('screen').innerHTML = screen
+    document.getElementById('tela').innerHTML = screen
 }
 
 function clear_ce(){
-    // Função para limpar o último números digitados no elemento com id "screen"
-    screen = document.getElementById('screen').innerHTML.trim()
+    // Função para limpar o último números digitados no elemento com id "tela"
+    screen = document.getElementById('tela').innerHTML.trim()
     if (screen != 0){
         if (screen.length == 1){
-            document.getElementById('screen').innerHTML = 0
+            document.getElementById('tela').innerHTML = 0
         }
         else {
-        document.getElementById('screen').innerHTML = screen.slice(0,-1)
+        document.getElementById('tela').innerHTML = screen.slice(0,-1)
     }
     
     }
@@ -48,24 +48,24 @@ function clear_ce(){
 function displayop(operator){
 
     // Função para exibir os operadores aritiméticos
-    // digitados no elemento com id "screen"
-    screen = document.getElementById('screen').innerHTML
+    // digitados no elemento com id "tela"
+    screen = document.getElementById('tela').innerHTML
     
     if ("+-*/".includes(operator.trim().slice(-1)) && !"+-*/".includes(screen.trim().slice(-1))){
         if (screen == 0){
-            document.getElementById('screen').innerHTML = operator
+            document.getElementById('tela').innerHTML = operator
         }
         else {
-            document.getElementById('screen').innerHTML += String(operator)
+            document.getElementById('tela').innerHTML += String(operator)
         }
         
     }
     else if ("()".includes(operator.trim().slice(-1)) && !"()".includes(screen.trim().slice(-1))){
         if(screen == 0){
-            document.getElementById('screen').innerHTML = operator
+            document.getElementById('tela').innerHTML = operator
         }
         else {
-            document.getElementById('screen').innerHTML += String(operator)
+            document.getElementById('tela').innerHTML += String(operator)
         }
     }
     
@@ -74,9 +74,9 @@ function displayop(operator){
 
 }
 function calc(){
-    // Função para calcular o resultado e exibir no elemento com id "screen"
-    screen = document.getElementById('screen').innerHTML
+    // Função para calcular o resultado e exibir no elemento com id "tela"
+    screen = document.getElementById('tela').innerHTML
     result = eval(screen)
-    document.getElementById('screen').innerHTML = result
+    document.getElementById('tela').innerHTML = result
     calculated = true
 }
